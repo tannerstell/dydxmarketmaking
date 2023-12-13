17084c3c88917f484fb99c4b5c89f30d058eff13
tannerstell <tanner314@gmail.com>
2023-12-12 23:43:45 -0800

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Killing existing Python processes.
 TASKKILL /F /IM python.exe /T
-call "C:\Users\tanne\PycharmProjects\dydx - 3.4\venv\Scripts\activate.bat"
+call "C:\enter\path\here\venv\Scripts\activate.bat"
 main.py
 
 pause
